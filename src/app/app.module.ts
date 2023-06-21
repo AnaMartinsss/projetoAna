f01b1210143b3ff73871994460374c888a863562
Beto <jrcampos82@gmail.com>
2023-06-21 16:20:03 -0400

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MemoriaComponent } from './memoria/memoria.component';
 import { CardapioComponent } from './cardapio/cardapio.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CardapioComponent } from './cardapio/cardapio.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
