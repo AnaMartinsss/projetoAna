15275202f955c94431d44301ef604eabea725747
Beto <jrcampos82@gmail.com>
2023-06-21 16:34:18 -0400

    att estrutura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CardapioComponent } from "./cardapio/cardapio.component";
 import { MemoriaComponent } from "./memoria/memoria.component";
+import { InicioComponent } from "./inicio/inicio.component";
 
 const routes: Routes = [
+  { path: "/", component: InicioComponent },
   { path: "jogo", component: MemoriaComponent },
   { path: "cardapio", component: CardapioComponent },
+  { path: "", redirectTo: "/", pathMatch: "full" },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { MemoriaComponent } from './memoria/memoria.component';
 import { CardapioComponent } from './cardapio/cardapio.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MemoriaComponent,
-    CardapioComponent
+    CardapioComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
