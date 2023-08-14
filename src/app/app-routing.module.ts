b5dec663a2b23cbf701e476e77109124b9cee183
AnaMartinsss <127670192+AnaMartinsss@users.noreply.github.com>
2023-08-14 15:50:19 -0300

    att cardapio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from "@angular/router";
 import { CardapioComponent } from "./cardapio/cardapio.component";
 import { MemoriaComponent } from "./memoria/memoria.component";
 import { InicioComponent } from "./inicio/inicio.component";
+import { LoginComponent } from "./components/login/login.component";
 
 const routes: Routes = [
   { path: "inicio", component: InicioComponent },
+  { path: "login", component: LoginComponent },
   { path: "jogo", component: MemoriaComponent },
   { path: "cardapio", component: CardapioComponent },
   { path: "", redirectTo: "inicio", pathMatch: "full" },
