12a8028520c966131fde49b2530e9932beffb8ec
AnaMartinsss <127670192+AnaMartinsss@users.noreply.github.com>
2023-09-01 11:26:21 -0300

    att

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,4 +5,9 @@ import { Component } from "@angular/core";
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  login() {
+    console.log("oi");
+  }
+}
+
