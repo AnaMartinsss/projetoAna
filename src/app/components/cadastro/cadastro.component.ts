1683d1b89bb8ba17ce19fc45eecbd3278960f7b1
AnaMartinsss <martinsanalaura108@gmail.com>
2023-09-22 14:22:16 -0300

    att.

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -8,3 +8,16 @@ import { Component } from '@angular/core';
 export class CadastroComponent {
 
 }
+
+const signtiplutton= document.getElementById("signup");
+const signInButton= document.getElementById("signIn"); 
+const container= document.getElementById("container");
+
+signInButton?.addEventListener('click',() => {
+container?.classList.add("right-panel-active");  
+})
+
+signInButton?.addEventListener('click',() => {
+  container?.classList.remove("right-panel-active");  
+  })
+
