c48053ced1f6608d3cd61c842fa9e36ca7632fcd
Beto <jrcampos82@gmail.com>
2023-11-27 15:44:06 -0400

    Fix form import and update component styles

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { CardapioComponent } from "./components/cardapio/cardapio.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { InicioComponent } from "./components/inicio/inicio.component";
 import { LoginComponent } from "./components/login/login.component";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CadastroComponent } from "./components/cadastro/cadastro.component";
 import { PagamentoComponent } from "./components/pagamento/pagamento.component";
 import { AplicativoComponent } from "./components/aplicativo/aplicativo.component";
@@ -17,7 +17,7 @@ import { EntregaComponent } from "./components/entrega/entrega.component";
 import { DescricaoComponent } from "./components/descricao/descricao.component";
 import { AdminPageComponent } from "./components/admin-page/admin-page.component";
 import { NgApexchartsModule } from "ng-apexcharts";
-import { ReservaComponent } from './components/reserva/reserva.component';
+import { ReservaComponent } from "./components/reserva/reserva.component";
 @NgModule({
   declarations: [
     AppComponent,
@@ -40,6 +40,7 @@ import { ReservaComponent } from './components/reserva/reserva.component';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     NgApexchartsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/components/descricao/descricao.component.ts
+++ frontend/src/app/components/descricao/descricao.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-descricao',
-  templateUrl: './descricao.component.html',
-  styleUrls: ['./descricao.component.css']
+  selector: "app-descricao",
+  templateUrl: "./descricao.component.html",
+  styleUrls: ["./descricao.component.css"],
 })
 export class DescricaoComponent {
+  guaca: number;
+  bacon: number;
+  tomate: number;
+  queijo: number;
+  jalapeno: number;
+  preco: number;
 
+  constructor() {
+    this.guaca = 1;
+    this.bacon = 1;
+    this.tomate = 1;
+    this.queijo = 1;
+    this.jalapeno = 1;
+    this.preco = 20;
+  }
+  decrement(n: number) {
+    switch (n) {
+      case 1:
+        if (this.guaca >= 1) {
+          this.guaca--;
+          this.preco -= 2;
+        }
+        break;
+      case 2:
+        if (this.bacon >= 1) {
+          this.bacon--;
+          this.preco -= 2;
+        }
+        break;
+      case 3:
+        if (this.tomate >= 1) {
+          this.tomate--;
+          this.preco -= 1;
+        }
+        break;
+      case 4:
+        if (this.queijo >= 1) {
+          this.queijo--;
+          this.preco -= 3;
+        }
+        break;
+      case 5:
+        if (this.jalapeno >= 1) {
+          this.jalapeno--;
+          if (this.jalapeno >= 1) this.preco -= 0.5;
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  increment(n: number) {
+    switch (n) {
+      case 1:
+        if (this.guaca < 5) {
+          this.guaca++;
+          this.preco += 2;
+        }
+        break;
+      case 2:
+        if (this.bacon < 5) {
+          this.bacon++;
+          this.preco += 2;
+        }
+        break;
+      case 3:
+        if (this.tomate < 5) {
+          this.tomate++;
+          this.preco += 1;
+        }
+        break;
+      case 4:
+        if (this.queijo < 5) {
+          this.queijo++;
+          this.preco += 3;
+        }
+        break;
+      case 5:
+        if (this.jalapeno < 5) {
+          this.jalapeno++;
+          if (this.jalapeno > 1) this.preco += 0.5;
+        }
+        break;
+    }
+  }
 }
