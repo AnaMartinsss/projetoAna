f1bbc120d1539045723d7e52852171dbeb9512ac
AnaMartinsss <martinsanalaura108@gmail.com>
2023-12-04 22:22:52 -0300

    Problema na pag descricao resolvido

--- frontend/src/app/components/descricao/descricao.component.ts
+++ frontend/src/app/components/descricao/descricao.component.ts
@@ -26,27 +26,28 @@ export class DescricaoComponent {
       case 1:
         if (this.guaca >= 1) {
           this.guaca--;
-          this.preco -= 2;
+        //  this.preco -= 2;
+          if (this.guaca >= 1) this.preco -= 2;
         }
         break;
       case 2:
         if (this.bacon >= 1) {
           this.bacon--;
-          this.preco -= 2;
-          if (this.jalapeno >= 1) this.preco -= 2;
+         // this.preco -= 2;
+          if (this.bacon >= 1) this.preco -= 2;
         }
         break;
       case 3:
         if (this.tomate >= 1) {
           this.tomate--;
-          this.preco -= 1;
+         // this.preco -= 1;
           if (this.tomate >= 1) this.preco -= 1;
         }
         break;
       case 4:
         if (this.queijo >= 1) {
           this.queijo--;
-          this.preco -= 3;
+         // this.preco -= 3;
           if (this.queijo >= 1) this.preco -= 3;
         }
         break;
@@ -67,25 +68,29 @@ export class DescricaoComponent {
       case 1:
         if (this.guaca < 5) {
           this.guaca++;
-          this.preco += 2;
+          //this.preco += 2;
+          if (this.guaca > 1) this.preco += 2;
         }
         break;
       case 2:
         if (this.bacon < 5) {
           this.bacon++;
-          this.preco += 2;
+          //this.preco += 2;
+          if (this.bacon > 1) this.preco += 2;
         }
         break;
       case 3:
         if (this.tomate < 5) {
           this.tomate++;
-          this.preco += 1;
+          //this.preco += 1;
+          if (this.tomate > 1) this.preco += 1;
         }
         break;
       case 4:
         if (this.queijo < 5) {
           this.queijo++;
-          this.preco += 3;
+          //this.preco += 3;
+          if (this.queijo > 1) this.preco += 3;
         }
         break;
       case 5:
